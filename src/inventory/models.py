1091dfc735363b21423f2e9655b13bf02e3f2d00
MathieuDlp <contact.mathieudelepine@gmail.com>
2022-03-31 16:57:19 +0200

    add 4 models in models.py

--- src/inventory/models.py
+++ src/inventory/models.py
@@ -1,3 +1,25 @@
+from tkinter import CASCADE
 from django.db import models
 
 # Create your models here.
+class Ingredients(models.Model):
+    name = models.CharField(max_length=35)
+    quantity = models.IntegerField()
+    unit = models.CharField(max_length=10)
+    unit_price = models.FloatField()
+
+class MenuItem(models.Model):
+    title = models.CharField(max_length=35)
+    price = models.FloatField()
+
+class RecipeRequirement(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
+    quantity = models.FloatField()
+
+class Purchase(models.Model):
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.SET_NULL, null=True)
+    timestamp = models.DateTimeField(auto_now=True)
+
+
+    
