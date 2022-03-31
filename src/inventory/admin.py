8c57632a4b464774150f4cd1b16cc961bb484173
MathieuDlp <contact.mathieudelepine@gmail.com>
2022-03-31 21:25:25 +0200

    configure admin section + add return method on models objects

--- src/inventory/admin.py
+++ src/inventory/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
+from .models import Ingredients, MenuItem, RecipeRequirement, Purchase
+
 # Register your models here.
+admin.site.register(Ingredients)
+admin.site.register(MenuItem)
+admin.site.register(RecipeRequirement)
+admin.site.register(Purchase)
--- src/inventory/models.py
+++ src/inventory/models.py
@@ -4,22 +4,33 @@ from django.db import models
 # Create your models here.
 class Ingredients(models.Model):
     name = models.CharField(max_length=35)
-    quantity = models.IntegerField()
+    quantity = models.FloatField()
     unit = models.CharField(max_length=10)
     unit_price = models.FloatField()
+
+    def __str__(self):
+        return self.name
 
 class MenuItem(models.Model):
     title = models.CharField(max_length=35)
     price = models.FloatField()
 
+    def __str__(self):
+        return self.title
+
 class RecipeRequirement(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
     ingredient = models.ForeignKey(Ingredients, on_delete=models.CASCADE)
     quantity = models.FloatField()
+    
+    def __str__(self):
+        return self.menu_item.title + " - " +self.ingredient.name
 
 class Purchase(models.Model):
     menu_item = models.ForeignKey(MenuItem, on_delete=models.SET_NULL, null=True)
-    timestamp = models.DateTimeField(auto_now=True)
+    timestamp = models.DateTimeField()
 
+    def __str__(self):
+        return self.menu_item.title + " - " + self.timestamp.strftime("%d/%m/%Y - %H:%M:%S")
 
     
