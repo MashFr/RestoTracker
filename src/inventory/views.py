a87b85782c359e4d2e515fda5e8ab56ffa1c4344
MathieuDlp <contact.mathieudelepine@gmail.com>
2022-04-01 10:13:28 +0200

    create templates and views for home and ingredientList

--- src/inventory/views.py
+++ src/inventory/views.py
@@ -1,3 +1,14 @@
+import imp
+from msilib.schema import ListView
 from django.shortcuts import render
+from .models import Ingredients, MenuItem, Purchase
+from django.views.generic import ListView
 
 # Create your views here.
+
+def home(request):
+    return render(request, "inventory/home.html")
+
+class IngredientList(ListView):
+    model = Ingredients
+    template_name = "inventory/ingredient_list.html"
